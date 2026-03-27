854e8893a36cd94fa64f55fa9e5662e2dafe958b
Gamedosa <gabriel20melo@gmail.com>
2026-03-26 23:11:21 -0300

    feat: adding terraform variables and provider.

---
+++ bootstrap-infra-script/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region"{
+    type = string
+    default = "us-east-1"
+}
+
+variable "github_repo"{
+    type = string
+}
+
+variable "github_branch"{
+    type = string
+    default = "main"
+}
+
+variable "github_org" {
+  type = string
+}
+
+variable "project_name"{
+    type = string
+    default = "aws-governance-lab"
+}
+
+variable "owner" {
+  type    = string
+  default = "gabriel"
+}
